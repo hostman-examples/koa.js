0b7d454207f4313074c729cca6feb479c3203bb6
Alexander Boykov <alexanderboykov@MacBook-Pro.local>
2020-06-04 17:49:28 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+const koa = require('koa')
+const koaRouter = require('koa-router')
+
+const app = new koa()
+const router = new koaRouter()
+
+router.get('koala', '/', (ctx) => {
+  ctx.body = "Welcome! To the Koala Book of Everything!"
+})
+
+app.use(router.routes())
+  .use(router.allowedMethods())
+
+app.listen(3000, () => console.log('running on port 3000'))
